36f73b9c3fad2c7ea0c1bb77f9b09a361ea694f3
brice.santus@gmail.com <brice.santus@gmail.com@946c81de-8c55-36f9-3613-8eb291ae2c00>
2011-05-17 12:51:26 +0000

    Réparation des conneries de l'autre bolosse de Nikko

--- application/models/DbTable/Demande.php
+++ application/models/DbTable/Demande.php
@@ -4,6 +4,30 @@ class Application_Model_DbTable_Demande extends Zend_Db_Table_Abstract
 {
 
     protected $_name = 'demande';
+    
+    public function obtenirDemande($id)
+	{
+		$id = (int)$id;
+		$row = $this->fetchRow('Num_demande= ' . $id);
+		if (!$row) {
+			throw new Exception("Impossible de trouver l'enregistrement $id");
+		}
+		return $row->toArray();
+	}
+	public function ajouterDemande()
+	{
+		//$data = array('artiste' => $artiste,'titre' => $titre,);
+		//$this->insert($data);
+	}
+	public function modifierDemande()
+	{
+		//$data = array('artiste' => $artiste,'titre' => $titre,);
+		//$this->update($data, 'id = '. (int)$id);
+	}
+	public function supprimerDemande($id)
+	{
+		$this->delete('Num_demande =' . (int)$id);
+	}
 
 	public function ajouterDemandeInformation($identifiant, $num_siret, $e_mail, $password, $adresse, $telephone, $nombre_employes)
     {
