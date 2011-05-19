060c787385c74e0e4989cf00ce5b6cd8b4ba0b15
domingues.nicolas <domingues.nicolas@946c81de-8c55-36f9-3613-8eb291ae2c00>
2011-05-19 13:28:59 +0000

    Bootstrap.php for JP

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,18 +2,23 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-	protected function _initView()
+	protected function _initViewHelpers()
 		{
+			$this->bootstrap('layout');
+			$layout = $this->getResource('layout');
+			$view = $layout->getView();
+			
+			$view->setHelperPath(APPLICATION_PATH.'/helpers', '');
+			ZendX_jQuery::enableView($view);
+			
 			// Initialize view
-			$view = new Zend_View();
-			$view->doctype('XHTML1_STRICT');
-			$view->headMeta()->appendHttpEquiv('Content-Type',
+			//$view->headMeta()->appendHttpEquiv('Content-Type',
 											   'text/html; charset=utf-8');
-			$view->addHelperPath('Zend/Dojo/View/Helper/', 'Zend_Dojo_View_Helper');
-			$view->addHelperPath('application/views/helpers/', 'App_View_Helper');
-			$view->addHelperPath('ZendX/JQuery/View/Helper', 'ZendX_JQuery_View_Helper');
-			Zend_Dojo::enableView($view);
-			Zend_View_Helper_PaginationControl::setDefaultViewPartial('pagination.phtml');
+			//$view->addHelperPath('Zend/Dojo/View/Helper/', 'Zend_Dojo_View_Helper');
+			//$view->addHelperPath('application/views/helpers/', 'App_View_Helper');
+			//$view->addHelperPath('ZendX/JQuery/View/Helper', 'ZendX_JQuery_View_Helper');
+			//Zend_Dojo::enableView($view);
+			//Zend_View_Helper_PaginationControl::setDefaultViewPartial('pagination.phtml');
 			// Add it to the ViewRenderer
 			$viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper('ViewRenderer');
 			$viewRenderer->setView($view);
