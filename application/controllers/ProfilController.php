b11e673b68027be04f8ab0f67a8489e0f79c7161
brice.santus@gmail.com <brice.santus@gmail.com@946c81de-8c55-36f9-3613-8eb291ae2c00>
2011-05-20 08:30:27 +0000

    Avancement page Profil

--- application/controllers/ProfilController.php
+++ application/controllers/ProfilController.php
@@ -10,25 +10,82 @@ class ProfilController extends Zend_Controller_Action
 
     public function indexAction()
     {
-    	// Chargmenent du profil
-		$form = new Application_Form_Profil();
+    	// Chargmenent du formulaire profil Adherent
+		$form = new Application_Form_ProfilAdherent();
 		$this->view->form = $form;
+		
+		//Récupération des droits de l'utilisateur
+		$droits = Zend_Auth::getInstance ()->getIdentity()->Droits;
+		
+		//Si le formulaire est validé en post
 		if ($this->getRequest()->isPost()) {
+			
+			//Récupération des valeurs POST
 			$formData = $this->getRequest()->getPost();
+			
+			//Si les valeurs sont valides
 			if ($form->isValid($formData)) {
-				$id = $form->getValue('id');
-				$nom = $form->getValue('nom');
-				$prenom = $form->getValue('prenom');
-				$num_SS = $form->getValue('num_SS');
-				$telephone = $form->getValue('telephone');
-				$adresse = $form->getValue('adresse');
-				$e_mail = $form->getValue('e_mail');
-									
-				$this->_helper->redirector('index');
+				
+				//Cas d'un Adhérent
+				if($droits == 1){
+					$id = $form->getValue('Id_utilisateur');
+					$nom = $form->getValue('nom');
+					$prenom = $form->getValue('prenom');
+					$num_SS = $form->getValue('numSS');
+					$telephone = $form->getValue('telephone');
+					$adresse = $form->getValue('adresse');
+					$e_mail = $form->getValue('e_mail');
+
+					//Mise à jour des données de l'Adhérent
+					$albums = new Application_Model_DbTable_Albums();
+					$albums->modifierAlbum($id, $artiste, $titre);
+				
+					$this->_helper->redirector('index');
+				}
+				
+				
 			} else {
 				$form->populate($formData);
 			}
-		} 
+		} else {
+			$id = $this->_getParam('id', -1);
+			if ($id > -1) {
+				
+				
+				//Affichage du formulaire correspondant au type d'utilisateur
+				if($droits == 0){
+					//Si l'utilisateur est un adhérent
+					$form = new Application_Form_ProfilAdherent();
+					$this->view->form = $form;
+					$adhérent = new  Application_Model_DbTable_Adherent();
+					$form->populate($adhérent->obtenirAdherent($id));
+				}elseif($droits == 2){
+					//Si l'utilisateur est une entreprise
+					/*$form = new Application_Form_ProfilEntreprise();
+					$this->view->form = $form;
+					$entreprise = new  Application_Model_DbTable_Entreprise();
+					$form->populate($entreprise->obtenirEntreprise($id));*/
+				}elseif($droits == 3){
+					//Si l'utilisateur est une Employé caisse
+					/*$form = new Application_Form_ProfilEntreprise();
+					$this->view->form = $form;
+					$entreprise = new  Application_Model_DbTable_Entreprise();
+					$form->populate($entreprise->obtenirEntreprise($id));*/
+				}elseif($droits == 4){
+					//Si l'utilisateur est une autre Caisse
+					/*$form = new Application_Form_ProfilEntreprise();
+					$this->view->form = $form;
+					$entreprise = new  Application_Model_DbTable_Entreprise();
+					$form->populate($entreprise->obtenirEntreprise($id));*/
+				}elseif($droits == 5){
+					//Si l'utilisateur est une CNAV
+					/*$form = new Application_Form_ProfilEntreprise();
+					$this->view->form = $form;
+					$entreprise = new  Application_Model_DbTable_Entreprise();
+					$form->populate($entreprise->obtenirEntreprise($id));*/
+				}
+			}
+		}
     }
 
 
--- application/models/DbTable/Utilisateur.php
+++ application/models/DbTable/Utilisateur.php
@@ -4,6 +4,8 @@ class Application_Model_DbTable_Utilisateur extends Zend_Db_Table_Abstract
 {
 
     protected $_name = 'utilisateur';
+    
+	
 
 
 }
