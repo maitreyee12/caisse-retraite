37ac584b72fd13305eef97aed18e466637793278
brice.santus@gmail.com <brice.santus@gmail.com@946c81de-8c55-36f9-3613-8eb291ae2c00>
2011-05-20 09:52:59 +0000

    Page Profil: formulaire informations personnelles pour: Entreprise et Adhérent

--- trunk/application/models/DbTable/Entreprise.php
+++ trunk/application/models/DbTable/Entreprise.php
@@ -5,6 +5,14 @@ class Application_Model_DbTable_Entreprise extends Zend_Db_Table_Abstract
 
     protected $_name = 'entreprise';
 
-
+	public function obtenirEntreprise($Id_utilisateur)
+	{
+		$Id_utilisateur = (int)$Id_utilisateur;
+		$row = $this->fetchRow('Id_utilisateur = ' . $Id_utilisateur);
+		if (!$row) {
+			throw new Exception("Impossible de trouver l'enregistrement $Id_utilisateur");
+		}
+		return $row->toArray();
+	}
 }
 
