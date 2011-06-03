75d06b9c2621f531b59f1e1774ab68b5aa0c07fa
brice.santus@gmail.com <brice.santus@gmail.com@946c81de-8c55-36f9-3613-8eb291ae2c00>
2011-06-03 15:29:43 +0000

    Profil - Affichage de la liste des période + infos carrière + liens vers les documents/période

--- application/controllers/PeriodeController.php
+++ application/controllers/PeriodeController.php
@@ -16,12 +16,8 @@ class PeriodeController extends Zend_Controller_Action
     public function afficherPeriodeAction()
     {
     	//On récupère l'id de l'utilisateur
-        $id_utilisateur =  $this->_getParam("id");
+        $id_carriere =  $this->_getParam("id");
         
-        //On récupère la carrière du salarié
-        $carriere = new Application_Model_DbTable_Carriere();
-        $id_carriere = $carriere->getCarriere($id_utilisateur)->Id_carriere;
-	
         //On récupére les périodes
         $periodes = new Application_Model_DbTable_Periode();
 		$this->view->Periodes = $periodes->obtenirPeriodes($id_carriere);
