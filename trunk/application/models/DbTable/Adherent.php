88be5f11e37d02fd93397599d3a1f80dd78f4933
chapel.jeanphilippe@gmail.com <chapel.jeanphilippe@gmail.com@946c81de-8c55-36f9-3613-8eb291ae2c00>
2011-06-08 18:41:54 +0000

    Ajout des modules recherche

--- trunk/application/models/DbTable/Adherent.php
+++ trunk/application/models/DbTable/Adherent.php
@@ -14,6 +14,45 @@ class Application_Model_DbTable_Adherent extends Zend_Db_Table_Abstract
 		}
 		return $row->toArray();
 	}
+
+	public function rechercheAdherent($id, $nom, $prenom, $num_ss, $tel, $email, $adresse, $status)
+		{
+			$query = $this->select(); 
+				if($id != null && $id != "")
+					{
+						$query->where('Id_utilisateur = ?', $id);
+					}
+				if($nom != null && $nom != "")
+					{
+						 $query->where('Nom LIKE ?', '%'.$nom.'%');
+					}
+				if($prenom != null && $prenom != "")
+					{
+						 $query->where('Prenom LIKE ?', '%'.$prenom.'%');
+					}
+				if($num_ss != null && $num_ss != "")
+					{
+						 $query->where('Num_SS = ?', $num_ss);
+					}		
+				if($tel != null && $tel != "")
+					{
+						 $query->where('Telephone = ?', $tel);
+					}		
+				if($email != null && $email != "")
+					{
+						 $query->where('E_mail = ?', $email);
+					}		
+				if($adresse != null && $adresse != "")
+					{
+						 $query->where('Adresse LIKE ?', '%'.$adresse.'%');
+					}		
+				if($status != null && $status != "")
+					{
+						 $query->where('Statut = ?', $status);
+					}							
+			return $this->fetchAll($query);
+		}
+
 	public function modifierAdhérent($Id_utilisateur, $nom, $prenom, $num_SS, $telephone, $adresse, $e_mail){
 		
 		$data = array(
