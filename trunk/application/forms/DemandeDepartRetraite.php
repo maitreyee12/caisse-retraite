fe2ec90360ba8ef4fc34d2c18d06f6dfa1f22c8c
brice.santus@gmail.com <brice.santus@gmail.com@946c81de-8c55-36f9-3613-8eb291ae2c00>
2011-06-12 11:16:21 +0000

    Début travaux demande départ en retraite.

--- trunk/application/forms/DemandeDepartRetraite.php
+++ trunk/application/forms/DemandeDepartRetraite.php
@@ -1,11 +1,52 @@
 <?php
 
-class Application_Form_DemandeDepartRetraite extends Zend_Form
+class Application_Form_DemandeDepartRetraite extends ZendX_JQuery_Form
 {
 
     public function init()
     {
-        /* Form Elements & Other Definitions Here ... */
+         //Titre du formulaire
+		$this->setName('demande_depart_retraite');
+
+		//ID
+		$id = new Zend_Form_Element_Hidden('Num_demande');
+		$id
+				->addFilter('Int');
+
+		//DATE				
+		$date = new ZendX_JQuery_Form_Element_DatePicker('Date',
+						array('jQueryParams' => array('dateFormat' => 'dd/mm/yy',
+                                                  'changeMonth' => 'true',
+                                                  'changeYear' => 'true',
+                                                  'yearRange' => '-20:0',
+                                                  'minDate' => '-20Y',
+                                                  'maxDate' => '0')));
+		$date ->setLabel('Date de départ souhaitée :');
+		
+		
+		//COMMENTAIRE
+		$commentaire = new Zend_Form_Element_Textarea('Commentaires');
+		$commentaire
+				->setLabel('Votre demande :')
+				->addFilter('StripTags')
+				->addFilter('StringTrim')
+				->setAttrib('cols', '80')
+				->setAttrib('rows', '10')
+				->setRequired(true)
+				->setAttrib('maxlength', '2048');
+		
+		$envoyer = new Zend_Form_Element_Submit('envoyer');
+		$envoyer
+					->setAttrib('id', 'boutonenvoyer')
+					->setLabel('Envoyer la demande');
+		
+		$this->addElements(array(
+											$id,
+											$date,
+											$commentaire,
+											$envoyer)
+								);
+		
     }
 
 
