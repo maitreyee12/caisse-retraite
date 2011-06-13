94d6c123aa91e94c0d6ccb5216c31179032c344e
brice.santus@gmail.com <brice.santus@gmail.com@946c81de-8c55-36f9-3613-8eb291ae2c00>
2011-06-13 13:46:14 +0000

    Ajout au profil du module de Droit au départ, préparation du module de liquidation de la retraite Création du forumalaire de demande de départ en retraite Création d'un fichier XML de paramétrage

--- trunk/application/models/DbTable/Carriere.php
+++ trunk/application/models/DbTable/Carriere.php
@@ -9,6 +9,11 @@ class Application_Model_DbTable_Carriere extends Zend_Db_Table_Abstract
     {
 		return $this->fetchRow($this->select()->where('Id_utilisateur = ?', $id_utilisateur));
 	}
+	public function getNombreTrimestre($Id_utilisateur)
+	{
+		$row = $this->fetchRow($this->select(array('Trimestre_cumul'))->where('Id_utilisateur = ?', $Id_utilisateur));
+		return $row->Trimestre_cumul;
+	}
 
 
 }
