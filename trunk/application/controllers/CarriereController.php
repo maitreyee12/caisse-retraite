32114e56aaba8a0375ad568d67eb4bd9d704bf5d
brice.santus@gmail.com <brice.santus@gmail.com@946c81de-8c55-36f9-3613-8eb291ae2c00>
2011-06-17 12:18:34 +0000

    Gestion de la liquidation de la retraite

--- trunk/application/controllers/CarriereController.php
+++ trunk/application/controllers/CarriereController.php
@@ -21,9 +21,11 @@ class CarriereController extends Zend_Controller_Action
            	$carriere = new Application_Model_DbTable_Carriere();
         	$this->view->carriere = $carriere->getCarriere($id_utilisateur);
         
-    	}
+        }	
     }
-    public function afficherDroitAction(){
+
+    public function afficherDroitAction()
+    {
     	
     	//Ouverture du fichier de configuration pour validation des paramètres
     	$dom = new DomDocument;
@@ -63,9 +65,149 @@ class CarriereController extends Zend_Controller_Action
             	$this->view->validDemande = false;
             	$this->view->dateDemande = null;
             }
-    	}
+        }
+    	
     }
+
+    public function afficherPlanAction()
+    {
+    	
+    //Ouverture du fichier de configuration pour validation des paramètres
+    	$dom = new DomDocument;
+  		$dom->load(APPLICATION_PATH."/configs/param.xml");
+  		
+  		//On récupère les paramètes légauc: Age minimum et nombre de trimestres
+  		$legalAgeMin = $dom->getElementsByTagName('agemin')->item(0)->nodeValue;
+  		$legalNombreTrimestre = $dom->getElementsByTagName('trimestre')->item(0)->nodeValue;
+     	$plafond_secu = $dom->getElementsByTagName('plafond')->item(0)->nodeValue;
+     	
+     	
+    	$id_utilisateur = $this->_getParam('id',-1);
+        
+        if($id_utilisateur != -1){
+        	//On récupère les informations de l'adhérent
+        	$adhérent = new Application_Model_DbTable_Adherent();
+        	$carriere = new Application_Model_DbTable_Carriere();
+        	$demande = new Application_Model_DbTable_Demande();
+        	
+            $age = $adhérent->getAge($id_utilisateur);
+            $trimestres = $carriere->getNombreTrimestre($id_utilisateur);
+            $demande_depart = $demande->getDemandeDepart($id_utilisateur);
+            
+            $this->view->Id_utilisateur = $id_utilisateur;
+            
+            if ($age >= $legalAgeMin)
+            	$this->view->validAge = true;
+            else 
+            	$this->view->validAge = false;
+            	
+            if($trimestres >= $legalNombreTrimestre)
+            	$this->view->validTrim = true;
+            else
+            	$this->view->validTrim = false;
+            	
+            if($demande_depart != null){
+            	$this->view->validDemande = true;
+            	$this->view->demande_depart = $demande_depart;
+            }else{
+            	$this->view->validDemande = false;
+            	$this->view->demande_depart = null;
+            }
+            if($age >= $legalAgeMin && $trimestres >= $legalNombreTrimestre && $demande_depart != null){
+            	//Récupération infos carrière
+            	$this->view->adherent = $adhérent->obtenirAdherent($id_utilisateur);
+        		$info_carriere = $carriere->getCarriere($id_utilisateur);
+        		$this->view->plancarriere = $info_carriere;
+        			
+            	//Récupération meilleures années
+            	$periode = new Application_Model_DbTable_Periode();
+            	$annees = $periode->getAnneePlanRetraite($info_carriere->Id_carriere);
+            	$this->view->annees = $annees;
+            	
+            	//Récupération des Points ARRCO/AGIRC de l'adhérent
+            	$points_ARRCO = $periode->getSommeARRCO($info_carriere->Id_carriere);
+            	$this->view->points_ARRCO = $points_ARRCO;
+            	$points_AGIRC = $periode->getSommeAGIRC($info_carriere->Id_carriere);
+            	$this->view->points_AGIRC = $points_AGIRC;
+            	
+            	//Récupération de la valeur des points ARRCO/AGIRC
+            	$arrco = $dom->getElementsByTagName('point_arrco')->item(0)->nodeValue;
+            	$this->view->arrco = $arrco;
+            	$agirc = $dom->getElementsByTagName('point_agirc')->item(0)->nodeValue;
+            	$this->view->agirc = $agirc;
+            	
+            	//Calcul salaire moyen
+            	$salaire_total=0;
+            	foreach($annees as $keys => $value):
+            		$salaire_total +=$value;
+            	endforeach;
+            	$salaire_moyen = $salaire_total/5/12;
+            	
+            	$this->view->salaire_moyen = round($salaire_moyen,2);
+            	if($salaire_moyen <= $plafond_secu){
+            		$this->view->salaire_plafonne = $salaire_moyen;
+            	}else{
+            		$this->view->salaire_plafonne = $plafond_secu;
+            	}
+            	//Calcul du montant
+        		 
+            }
+        }
+        
+    }
+
+    public function departRetraiteAction()
+    {
+    	$Id_utilisateur = $this->_getParam('id',-1);
+        
+        if($Id_utilisateur != -1){
+        	//Changer Statut salarié
+        	$adherent = new Application_Model_DbTable_Adherent();
+        	$adherent->retraiteAdherent($Id_utilisateur);
+	        
+        	//Calcul du montant retraite
+        	//Récupération des Points ARRCO/AGIRC de l'adhérent
+        	$carriere = new Application_Model_DbTable_Carriere();
+        	$info_carriere = $carriere->getCarriere($Id_utilisateur);
+        	$periode = new Application_Model_DbTable_Periode();
+           	$points_ARRCO = $periode->getSommeARRCO($info_carriere->Id_carriere);
+            $points_AGIRC = $periode->getSommeAGIRC($info_carriere->Id_carriere);
+
+            
+            //Récupération de la valeur des points ARRCO/AGIRC
+            //Ouverture du fichier de configuration pour validation des paramètres
+    		$dom = new DomDocument;
+  			$dom->load(APPLICATION_PATH."/configs/param.xml");
+  		    $arrco = $dom->getElementsByTagName('point_arrco')->item(0)->nodeValue;
+            $agirc = $dom->getElementsByTagName('point_agirc')->item(0)->nodeValue;
+            
+            //Montant et date de début
+            $Montant = round($arrco*$points_ARRCO+$agirc*$points_AGIRC,2);
+        	$date = mktime(0,0,0,date("m")+6,date("d"),date("Y"));
+        	$Date_debut = date('Y-m-d',$date);
+        	            
+        	//Créer retraite
+        	$retraite = new Application_Model_DbTable_Retraite();
+        	$retraite->addRetraite($Montant, $Date_debut);
+        	
+        	//Modifier carrière
+        	$Id_retraite = $retraite->getDerniereRetraite();
+        	$carriere->departRetraite($Id_utilisateur, $Date_debut, $Id_retraite);
+        	
+        	//Changer Etat demande départ en retraite
+        	$demande = new Application_Model_DbTable_Demande();
+        	$demande_depart = $demande->getDemandeDepart($Id_utilisateur);
+        	$Id_demande = $demande_depart->Id_demande;
+        	$demande->modifierEtatDemande($Id_demande, 2);
+        }
+    }
+
+
 }
+
+
+
+
 
 
 
--- trunk/application/models/DbTable/Periode.php
+++ trunk/application/models/DbTable/Periode.php
@@ -25,6 +25,106 @@ class Application_Model_DbTable_Periode extends Zend_Db_Table_Abstract
 				);
 			$this->insert($data);
 		}
+public function getAnneePlanRetraite($id_carriere)
+	{
+		//Récupération de la date du début de carrière et fin de carrière
+		$row =  $this->fetchRow($this->select(array('MIN(Date_debut)'))->where('Id_carriere = ?', $id_carriere)->order('Date_debut ASC'));
+		$date_debut = $row->Date_debut;
+		$row =  $this->fetchRow($this->select(array('MAX(Date_fin)'))->where('Id_carriere = ?', $id_carriere)->order('Date_fin DESC'));
+		$date_fin = $row->Date_fin;
+		
+		//Boucle debut while(date début< date fin dernière période)
+		while($date_debut < $date_fin){
+			
+			
+			//On fabrique la première date
+			$temp_date = explode('-',$date_debut);
+			$date_debut_temp = $temp_date[0].'-01-01';
+			//On récupère l'année courante
+			$annee = $temp_date[0];
+			//On créé la date de fin de la première année
+			$temp_date[0]=$temp_date[0]+1;
+			$date_fin_temp = $temp_date[0].'-01-01';
+			
+			
+						
+			//->Calculer salaire
+			//-->1)Trouver toutes les périodes where dd<Date début<df || dd<date fin<df
+			$row = $this->getPeriodeByDate($id_carriere,$date_debut_temp,$date_fin_temp);
+			
+			//-->2)Somme (salaire*prorata couverture)
+			$salaire_annee = 0;
+			foreach($row as $periode) :
+				
+				if($date_debut_temp <= $periode->Date_debut && $periode->Date_debut < $date_fin_temp)
+				{
+					$salaire = floatval($periode->Salaire_percu);
+					$nombre_jour = round((strtotime($date_fin_temp)-strtotime($periode->Date_debut))/(60*60*24));
+					$prorata = 100*$nombre_jour/365;	
+					//echo "</br>Cas 1 = Salaire de :".$salaire." pour ".$nombre_jour." jours avec un prorata de ".$prorata;
+						
+				}elseif ($date_debut_temp <= $periode->Date_fin && $periode->Date_fin < $date_fin_temp)
+				{
+					$salaire = floatval($periode->Salaire_percu);
+					$nombre_jour = round((strtotime($periode->Date_fin)-strtotime($date_debut_temp))/(60*60*24));
+					$prorata = 100*$nombre_jour/365;	
+					//echo "</br>Cas 2 = Salaire de :".$salaire." pour ".$nombre_jour." jours avec un prorata de ".$prorata;		
+				}
+				//On calcul le salaire de la période concernant l'année et on l'ajoute au salaire déjà existant
+				$salaire_annee += $salaire*$prorata/100;
+				$liste_annee[$annee] = floatval($salaire_annee);
+				//echo print_r($liste_annee)." ";
+			endforeach;
+			
+			//->Date début = date début + 1 ans
+			$date_debut = $date_fin_temp;
+		}
+		
+		//On trie le tableau par ordre décroissant de salaire perçu
+		//echo "</br>Liste_anne: ".print_r($liste_annee);
+		arsort($liste_annee);
+		//echo "</br>Asort: ".print_r($liste_annee);
+		for($i=0;$i<25;$i++){
+			list($key,$val) = each($liste_annee);
+			if($key !="" && $val !="")
+				$cinq_annee[$key] = round($val,2);
+		}
+		return $cinq_annee;
+		
+	}
+	public function getPeriodeByDate($id_carriere,$date_debut,$date_fin)
+	{
+		return $this->fetchAll($this->select()
+									->where('Id_carriere = ?', $id_carriere)
+									->where("Date_debut >= '$date_debut' AND Date_debut <= '$date_fin'")
+									->orwhere("Date_fin >= '$date_debut' AND Date_fin <= '$date_fin'")
+		);
+		
+	}
+	public function getSommeARRCO($id_carriere){
+		$liste = $this->fetchAll($this->select()
+									->where('Id_carriere = ?', $id_carriere));
+		$somme= 0;
+		
+		foreach ($liste as $elem){
+			
+			$somme+=$elem->Points_ARRCO;
+		}
+		return $somme;
+		
+	}
+	public function getSommeAGIRC($id_carriere){
+		$liste = $this->fetchAll($this->select()
+									->where('Id_carriere = ?', $id_carriere));
+		$somme= 0;
+		
+		foreach ($liste as $elem){
+			
+			$somme+=$elem->Points_AGIRC;
+		}
+		return $somme;
+	}
+
 
 	public function getDerniereId()
 		{
